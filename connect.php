57cbf5d76a3d8b3302ba8baf3ffc5ba4ad0dc015
Bharat Vishwakarma <bharatvishwakarma171@gmail.com>
2025-08-21 13:53:02 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 // mysql_connect
 // mysqli_connect
 // PDO
-$conn = mysqli_connect('localhost','root','','login');
+$conn = mysqli_connect('localhost','root','','crudoperation');
 // var_dump($conn);
 if(!$conn){
 //     echo "Connection Successfull";
@@ -11,5 +11,6 @@ if(!$conn){
 //     // echo "Connection Failed !";
     die("Connection Failed!".mysqli_connect_err());
 }
+
 
 ?> 
