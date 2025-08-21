c66441f8175e06b0b69d114d755b102b57d9041d
Bharat Vishwakarma <bharatvishwakarma171@gmail.com>
2025-08-21 13:54:40 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,23 +1,17 @@
 <?php
-require_once "connect.php";
-//  var_dump($_GET);
-//  echo $_GET['delete_id'];
-if (isset($_GET['delete_id_'])) {
-    $delete_id = $_GET['delete_id_'];
-    // echo $delete_id;
-    $delete_query = "DELETE FROM crud where id = $delete_id";
-    
-    $result = mysqli_query($conn,$delete_query);
-    if ($result) {
-          echo  "<script>alert('Record  deleted Successfully');</script>";
-          echo  "<script>window.open('display.php','_self');</script>";
-    }
 
-    else {
-        die(mysqli_query($result));
+include('connect.php');
+if(isset($_GET['delete_id'])){
+    $id=$_GET['delete_id'];
+    // echo $id;
+    $sql_delete="Delete from `crud` where id=$id";
+    $result=mysqli_query($con,$sql_delete);
+    if($result){
+        echo "<script>alert('Record deleted successfully')</script>";
+        echo "<script>window.open('display.php','_self')</script>";
+    }else{
+        die(mysqli_error($con));
     }
 }
-else {
-    die(mysqli_error($conn));
-}
+
 ?>
